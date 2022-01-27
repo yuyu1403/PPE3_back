e2d40b451e48c54d8efe38d414a4211eb9f8517e
Khoudou Nordine <nordine-khoudou@hotmail.fr>
2022-01-27 17:35:32 +0100

    backend

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+const pool = require("../config/database");
+
+module.exports = {
+    test: async (req, res) => {
+        const { prenom, nom, email, telephone, password, typeLigue} = req.body;
+        let connexion;
+        try {
+            connexion = await pool.getConnection();
+            const result = await connexion.query("CALL insertNewUser (?, ?, ?, ?, ?, ?)", [prenom, nom, email, telephone, password, typeLigue]);
+            return res.status(200).json({ success: result })
+        } catch (error) {
+            return res.status(400).json({ error: error.message});
+        } finally {
+            if (connexion) connexion.end()
+        }
+    }
+}
